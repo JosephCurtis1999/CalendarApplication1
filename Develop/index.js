3272ec0dcc16e16c896001d04d67efe79d0795f5
Joe Curtis <joecurtis9999@gmail.com>
2022-01-17 12:38:33 +0000

    Added variables for each hourly time using Jquery, referencing the ids in the html file

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,12 @@
 var presentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var presentHour = moment().format('h:mm:ss a');
 
+var nineAM = $('#9AM');
+var tenAM = $('#10AM');
+var elevenAM = $('#11AM');
+var twelveAM = $('#12AM');
+var onePM = $('#1PM');
+var twoPM = $('#2PM');
+var threePM = $('#3PM');
+var fourPM = $('#4PM');
+var fivePM = $('#5PM');
